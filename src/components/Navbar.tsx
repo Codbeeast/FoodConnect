2b907f62bc401c02bcd71de7168595059de795f1
Priyanshu <vishwakarmapriyanshu9099@gmail.com>
2025-06-13 23:01:05 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Avatar from './Avatar'
 const Navbar = () => {
   return (
    
-     <nav className="lg:w-[300px] w-[93vw] bg-black text-white flex px-4 py-2 mb-1  justify-around items-center">
+     <nav className="lg:w-full w-[93vw] bg-black text-white flex px-4 py-2 mb-1  justify-between items-center">
   <h1>FoodConnect</h1>
 
   <div className="flex gap-2 lg:gap-10">
