d867ef276e8484ba9d739c9595f9b9755dadc12b
Priyanshu <vishwakarmapriyanshu9099@gmail.com>
2025-06-13 22:03:59 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,14 +3,14 @@ import { img } from '.././images/Url'
 
 const Card = () => {
   return (
-    <div>
+    
       <div className=' lg:w-303 w-108 rounded-xl m-2  h-85 bg-black relative overflow-hidden'>
         <img
           src={img}
           alt=""
           className="absolute inset-0 w-full h-full object-center object-cover"
         />
-      </div>
+     
     </div>
   )
 }
